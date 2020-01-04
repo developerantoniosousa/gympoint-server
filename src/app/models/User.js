83707568a15ecbf0e3ee679c6ece99ba3e171b95
Antonio Sousa <developerantoniosousa@gmail.com>
2020-01-04 14:58:38 -0300

    Chore: add name field in user model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -5,6 +5,7 @@ class User extends Model {
   static init(sequelize) {
     super.init(
       {
+        name: Sequelize.STRING,
         email: Sequelize.STRING,
         password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
